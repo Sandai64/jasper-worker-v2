2fd2249a7fe103637d3d7a8d250e2df5104aa719
Erwan Egasse <contact@erwan.sh>
2022-10-17 12:53:33 +0200

    utils: add get_user_secrets & get_secrets_file_path methods

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: non_constant_identifier_names, constant_identifier_names
 
+import 'dart:convert';
+import 'dart:io' as io;
 import 'package:jasper_worker_v2/constants.dart';
 
 class Utilities
@@ -7,6 +9,37 @@ class Utilities
   static void print_welcome()
   {
     print("Jasper Worker (${ Constants.VERSION })");
-    print('------------------------------');
+    print('---------------------');
+    print('W: Warnings');
+    print('E: Errors');
+    print('I: Infos');
+    print('---------------------');
+  }
+
+  static String get_secrets_file_path()
+  {
+    // Are we running unpacked ?
+    if ( io.Platform.executable.endsWith('/bin/dart') )
+    {
+      return "../${ Constants.SECRETS_FILE_NAME }";
+    }
+
+    // Running compiled, file is alongside executable
+    return "./${ Constants.SECRETS_FILE_NAME }";
+  }
+
+  static Future<Map?> get_user_secrets() async
+  {
+    var fileExists = await io.File(get_secrets_file_path()).exists();
+
+    if ( fileExists )
+    {
+      String secrets_raw = await io.File(get_secrets_file_path()).readAsString();
+      Map<String, dynamic> user_secrets = json.decode(secrets_raw);
+
+      return user_secrets;
+    }
+
+    return null;
   }
 }
