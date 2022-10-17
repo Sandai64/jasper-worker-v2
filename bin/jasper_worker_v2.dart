4250c98a394818c91c08701efb5f35f2ea5f0fb5
Erwan Egasse <contact@erwan.sh>
2022-10-17 13:07:39 +0200

    Add secrets file check

--- bin/jasper_worker_v2.dart
+++ bin/jasper_worker_v2.dart
@@ -1,8 +1,18 @@
-// import 'package:jasper_worker_v2/jasper_worker_v2.dart' as jasper_worker_v2;
+// ignore_for_file: non_constant_identifier_names, constant_identifier_names
 
 import 'package:jasper_worker_v2/utilities.dart';
+import 'dart:io' as io;
 
 Future<void> main(List<String> arguments) async
 {
   Utilities.print_welcome();
+
+  Map<String, dynamic>? user_secrets = await Utilities.get_user_secrets();
+
+  if ( user_secrets == null )
+  {
+    print("E: Cannot read contents of '${ Utilities.get_secrets_file_path() }'");
+    io.exit(1);
+  }
+
 }
